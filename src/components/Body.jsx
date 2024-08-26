5b7337ac96ca5a99ad8cd21244faec6edfd30734
Ronita <rajeswarimahapatra40@gmail.com>
2024-08-26 12:09:15 +0530

    new comit

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -12,85 +12,125 @@ function getRandomColor() {
 }
 
 function Body() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [activePopup, setActivePopup] = useState(null);
 
-  const togglePopup = () => {
-    setIsOpen(!isOpen);
+  const togglePopup = (popupName) => {
+    if (activePopup === popupName) {
+      setActivePopup(null); 
+    } else {
+      setActivePopup(popupName); 
+    }
   };
 
   return (
-    <div id="btn-grp" className="d-flex">
-      <div className="flex-grow-1 p-5">
-        <div className='bg-light text-center text-dark border border-dark rounded'>
-          <h3>COMPONENT</h3>
-        </div>
-        <div className="d-flex gap-3 flex-wrap p-5 ml-40px">
-          <button 
-            type="button" 
-            className='btn text-white btn-overlay'
-            style={{
-              height: "250px",
-              width: "250px",
-              backgroundColor: getRandomColor(),
-              fontSize: "40px"
-            }}>
-            <span className="btn-text">LORA</span>
-            <div className="overlay"></div>
-          </button>
-          
-          <button 
-            type="button" 
-            className='btn text-white btn-overlay'
-            style={{
-              height: "250px",
-              width: "250px",
-              backgroundColor: getRandomColor(),
-              fontSize: "40px"
-            }}
-            onClick={togglePopup}
-          >
-            <span className="btn-text">rpm ENCODER</span>
-            <div className="overlay"></div>
-          </button>
-            {isOpen && (
-            <div className="popup">
-              <div className="popup-content">
-                <button>in rpm</button>
-                <button>out</button>
-                <button onClick={togglePopup}>Close</button>
-              </div>
-            </div>
-          )}
-          
-          <button 
-            type="button" 
-            className='btn text-white btn-overlay'
-            style={{
-              height: "250px",
-              width: "250px",
-              backgroundColor: getRandomColor(),
-              fontSize: "40px"
-            }}>
-            <span className="btn-text">SMPS</span>
-            <div className="overlay"></div>
-          </button>
-          
-          <button 
-            type="button" 
-            className='btn text-white btn-overlay'
-            style={{
-              height: "250px",
-              width: "250px",
-              backgroundColor: getRandomColor(),
-              fontSize: "40px"
-            }}>
-            <span className="btn-text">Proximity Sensor</span>
-            <div className="overlay"></div>
-          </button>
+    <div>
+      <div id="btn-grp" className={`d-flex ${activePopup ? 'blurred' : ''}`}>
+        <div className={`flex-grow-1 p-5`}>
+          <div className='bg-light text-center text-dark border border-dark rounded'>
+            <h3>COMPONENT</h3>
+          </div>
+          <div className="btn-grp d-flex gap-3 flex-wrap p-5 ml-40px">
+            <button 
+              type="button" 
+              className='btn text-white btn-overlay'
+              style={{
+                height: "250px",
+                width: "250px",
+                backgroundColor: getRandomColor(),
+                fontSize: "40px"
+              }}
+              onClick={() => togglePopup('lora')}
+            >
+              <span className="btn-text">LORA</span>
+              <div className="overlay"></div>
+            </button>
+
+            <button 
+              type="button" 
+              className='btn text-white btn-overlay'
+              style={{
+                height: "250px",
+                width: "250px",
+                backgroundColor: getRandomColor(),
+                fontSize: "40px"
+              }}
+              onClick={() => togglePopup('rpm')}
+            >
+              <span className="btn-text">rpm ENCODER</span>
+              <div className="overlay"></div>
+            </button>
+
+            <button 
+              type="button" 
+              className='btn text-white btn-overlay'
+              style={{
+                height: "250px",
+                width: "250px",
+                backgroundColor: getRandomColor(),
+                fontSize: "40px"
+              }}
+              onClick={() => togglePopup('smps')}
+            >
+              <span className="btn-text">SMPS</span>
+              <div className="overlay"></div>
+            </button>
+
+            <button 
+              type="button" 
+              className='btn text-white btn-overlay'
+              style={{
+                height: "250px",
+                width: "250px",
+                backgroundColor: getRandomColor(),
+                fontSize: "40px"
+              }}
+              onClick={() => togglePopup('proximity')}
+            >
+              <span className="btn-text">Proximity Sensor</span>
+              <div className="overlay"></div>
+            </button>
+          </div>
         </div>
       </div>
-      
-  
+
+      {/* Popup Content */}
+      {activePopup === 'lora' && (
+        <div className="popup">
+          <button className="close-btn" onClick={() => togglePopup(null)}>X</button>
+          <div className="popup-content">
+            <h4>Node value:</h4>
+            <input type="text" class="form-control" placeholder="First name"/>
+          </div>
+        </div>
+      )}
+
+      {activePopup === 'rpm' && (
+        <div className="popup">
+          <button className="close-btn" onClick={() => togglePopup(null)}>X</button>
+          <div className="popup-content">
+            <button>in rpm</button>
+            <button>out rpm</button>
+          </div>
+        </div>
+      )}
+
+      {activePopup === 'smps' && (
+        <div className="popup">
+          <button className="close-btn" onClick={() => togglePopup(null)}>X</button>
+          <div className="popup-content">
+            <h2>SMPS Popup Content</h2>
+          </div>
+        </div>
+      )}
+
+      {activePopup === 'proximity' && (
+        <div className="popup">
+          <button className="close-btn" onClick={() => togglePopup(null)}>X</button>
+          <div className="popup-content">
+            <h2>Proximity Sensor Popup Content</h2>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
