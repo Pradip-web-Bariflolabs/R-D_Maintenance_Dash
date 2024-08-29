da8f2c042acf51c54eef62eb88db47ac44b29870
Ronita <rajeswarimahapatra40@gmail.com>
2024-08-29 17:09:20 +0530

    add sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import '../assets/css/NavBar.css'; 
 
 function Navbar() {
   const [isConnected, setIsConnected] = useState(true);
@@ -10,29 +11,31 @@ function Navbar() {
   return (
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{height:"5rem"}}>
-        <div className="container-fluid d-flex align-items-center">
+        <div className="w-full container-fluid d-flex align-items-center">
           {/* Round circle with logo  */}
-          <div className="rounded-circle bg-light shadow d-flex justify-content-center align-items-center" 
+          {/* <div className="rounded-circle bg-light shadow d-flex justify-content-center align-items-center" 
                style={{ width: '80px', height: '80px', marginLeft: '3rem', marginTop: '2rem' }}>
-          </div>
+          </div> */}
 
-          <div className="collapse navbar-collapse justify-content-end" id="navbarText">
-            <div className="d-flex align-items-center">
-              <div className="">
-                <button className="navbar-text font-weight-normal text-decoration-underline p-0 mb-0" 
-                        style={{
-                            border: 'none',             
-                            fontSize: '2rem',           
-                            fontWeight: 'normal',       
-                            textDecoration: 'underline', 
-                            backgroundColor: 'transparent',                  
-                        }}>
-                  USER NAME
-                </button>
-                <p className="mb-0">
-                  MQTT CONN {dynamicImage}
-                </p>
-              </div>
+          <div className="collapse navbar-collapse justify-content-end">
+            <div className="d-flex align-items-center ">
+            <div className="">
+              <button
+                className=" p-0 mb-0 username-button"
+                style={{
+                  border: 'none',
+                  fontSize: '2rem',
+                  fontWeight: 'normal',
+                  textDecoration: 'underline',
+                  backgroundColor: 'transparent',
+                }}
+              >
+                USER NAME
+              </button>
+              <p className="mb-0">
+                MQTT CONN {dynamicImage}
+              </p>
+            </div>
             </div>
           </div>
         </div>
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,46 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import styled from 'styled-components';
-
-// Styled component for the button
-const CustomButton = styled.button`
-  width: 10rem;          
-  border: none;        
-  border-radius: 10px;    
-  font-size: 18px;
-  font-family: sans-serif;
-  text-decoration: none;
-  margin-bottom: 20px;
-  padding: 10px;
-  background-color: #f8f9fa; 
-  display: flex;
-  align-items: center;
-
-  &:hover {
-    background-color: #e0e0e0;
-  }
-
-  .icon {
-    margin-right: 10px; 
-  }
-`;
+import '../assets/css/SideBar.css';
 
 function SideBar() {
-  return (
-    <aside className="d-flex flex-column bg-light col-auto min-vh-100 border border-top-0" style={{ width: '200px' }}>
-      <div className="flex-grow-1" style={{ marginTop: '57px', marginLeft: '24px' }}>
-        <CustomButton>
-          <i className="bi bi-collection icon"></i> 
-          Dashboard
-        </CustomButton>
 
-        <CustomButton>
-          <i className="bi bi-box-arrow-right icon"></i>
-          Logout
-        </CustomButton>
-        
-      </div>
-    </aside>
+  const [isOpen, setIsOpen] = useState(true); 
+
+  const toggleOpen = () => {
+    setIsOpen(!isOpen); 
+  };
+
+  return (
+    <>
+    <button
+      className='toggle-btn'
+      onClick={toggleOpen}
+    >
+    <i className="bi bi-list "></i>
+    </button>
+
+    {isOpen && (
+      <aside className="d-flex flex-column bg-light col-auto min-vh-100 border border-top-0 ${isOpen ? 'open' : 'closed'}">
+        <div className="  flex-column flex-grow-1 side-bar" style={{ marginTop: '57px', marginLeft: '24px' }}>
+          <div className='side-btn'>
+            <i className="bi bi-collection icon"></i> 
+            Dashboard
+          </div>
+          <div className='side-btn'>
+            <i className="bi bi-box-arrow-right icon"></i>
+            Logout
+          </div>
+        </div>
+      </aside>
+      )}
+    </>
   );
 }
 
