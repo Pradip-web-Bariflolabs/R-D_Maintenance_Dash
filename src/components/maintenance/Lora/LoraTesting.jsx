d2898555c4bc3c2a4755292672de6715e6bbd080
chiku <codexchiku@gmail.com>
2024-08-30 18:12:20 +0530

    changes some styling in lora testing

--- src/components/maintenance/Lora/LoraTesting.jsx
+++ src/components/maintenance/Lora/LoraTesting.jsx
@@ -1,43 +1,42 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Chart from "../Chart"
-import './LoraTesting.css'
+import Chart from "../Chart";
 import { useLocation } from 'react-router-dom';
+import './LoraTesting.css';
+
 const LoraTesting = () => {
-  const location =useLocation();
-  const {state}=location;
-  const topicid=state?.data;
+  const location = useLocation();
+  const { state } = location;
+  const topicid = state?.data;
   console.log(topicid);
-  
-  
+
   return (
-    <>
-      {/* Device ID centered at the top */}
-      <div className="d-flex justify-content-center">
-        <p className="border border-primary border-3 mt-4 p-2 rounded fw-bold ">DeviceId: {topicid}</p>
+    <div className="container-fluid">
+      <div className='d-flex justify-content-center my-3'>
+        <p className="border border-primary border-2 px-3 py-1 rounded">
+          DeviceId: {topicid}
+        </p>
       </div>
 
-      <div className="container-fluid vh-100">
-        <div className="row h-100 d-flex justify-content-evenly">
-          {/* Left side with the chart on larger screens; on smaller screens, this will be on top */}
-          <div className="col-lg-6 col-12 d-flex justify-content-center align-items-center p-3 border border-primary border-3 rounded-5 " style={{ maxHeight: '85vh', maxWidth:'95vh' }}>
-            <Chart />
+      <div className='d-flex flex-wrap justify-content-around'>
+
+        <div className="col-lg-5 col-12 d-flex justify-content-center align-items-center  border border-primary border-3 rounded-5 mb-3" style={{ height: '80vh', maxWidth: '80%' }}>
+          <Chart />
+        </div>
+
+        <div className="col-lg-5 col-12 overflow-auto border border-primary border-3 rounded-5 hide-scrollbar mb-3" style={{ height: '80vh', maxWidth: '80%' }}>
+          {/* Random data for testing scroll */}
+          <div className="p-3 text-center">
+            {[...Array(50)].map((_, index) => (
+              <div key={index} className="mb-2 p-2 border border-gray-300">
+                Random Data {index + 1}
+              </div>
+            ))}
           </div>
 
-          {/* Right side with scrollable content; on smaller screens, this will be below the chart */}
-          <div className="col-lg-6 col-12 overflow-auto border border-primary border-3 rounded-5 hide-scrollbar" style={{ maxHeight: '85vh', maxWidth:'95vh' }}>
-            {/* Random data for testing scroll */}
-            <div className="p-3 text-center ">
-              {[...Array(50)].map((_, index) => (
-                <div key={index} className="mb-2 p-2 border border-gray-300 ">
-                  Random Data {index + 1}
-                </div>
-              ))}
-            </div>
-          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
